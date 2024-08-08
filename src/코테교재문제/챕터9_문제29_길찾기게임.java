1c0e89df435e3ea8bf2fcd4d16460782db1b77c0
나형준 <@211.202.61.97>
2024-08-08 15:31:45 +0900

    .

--- src/코테교재문제/챕터9_문제29_길찾기게임.java
+++ src/코테교재문제/챕터9_문제29_길찾기게임.java
@@ -77,13 +77,6 @@ public class 챕터9_문제29_길찾기게임 {
 		return nodeInfo;
 	}
 	
-	public static int[] preorder() {
-		
-	}
-	public static int[] postorder() {
-		
-	}
-	
 	public static void main(String[] args) {
 		int[][] nodeInfo = {{5,3}, {11,5}, {13,3}, {3,5}, {6,1}, {1,3}, {8,6}, {7,2}, {2,2}};
 		solution(nodeInfo);
