ee5b48b4d6a5ecab899cee16fa0b99830a067fc9
나형준 <@211.202.61.97>
2024-08-12 00:16:11 +0900

    .

--- src/백준/사이클게임_20040번.java
+++ src/백준/사이클게임_20040번.java
@@ -13,13 +13,26 @@ public class 사이클게임_20040번 {
 		set = new int[n]; // n만큼 집합 배열 생성
 		for(int i=0; i<n; ++i) set[i] = i; // 집합 배열의 각 부모를 자기 자신으로 설정(초기화)
 		
-		
+		int result=0;
+		for(int i=0; i<m; ++i) {
+			int c1 = command[i][0];
+			int c2 = command[i][1];
+			if(find(c1) == find(c2)) { // 사이클이 형성되면
+				result = i+1;
+				break;
+			}
+			union(c1,c2);
+		}
+		System.out.println(result);
 		scan.close();
 	}
 	public static int find(int n) {
-		if()
+		if(set[n] == n) return n;
+		return set[n] = find(set[n]);
 	}
 	public static void union(int n1, int n2) {
-		
+		int root1 = find(n1);
+		int root2 = find(n2);
+		if(root1!=root2) set[root2] = root1;
 	}
 }
